0cedd1fa4120abaa181ed75b55cfaf3b1f35b5af
Rocket Drew <rocketdrew@Rockets-MacBook-Air-2.local>
2022-03-03 21:02:50 -0500

    created lyrics table

---
+++ data_deliverable/lyrics_col.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# Create connection to database
+conn = sqlite3.connect('billboard.db')
+c = conn.cursor()
+
+lyrics_cmd = '''
+ALTER TABLE 
+	billboard
+ADD
+	lyrics text
+'''
+
+c.execute(lyrics_cmd)
+
+conn.commit()
+c.close()
+conn.close()
